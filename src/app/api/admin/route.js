4cae0ee89bce83e47cf235aadd5f15e006662e54
moeez <moeez@cytomate.net>
2024-11-20 16:16:29 +0500

    hhj

--- src/app/api/admin/route.js
+++ src/app/api/admin/route.js
@@ -1,32 +1,21 @@
-import { ObjectId } from "mongodb";
 import clientPromise from "../../../../lib/mongodb";
 
 export async function GET() {
   try {
     const client = await clientPromise;
     const db = client.db("BookingSys");
-
-    console.log("Fetching all client data");
     const clients = await db
       .collection("users")
       .find({ role: "client" })
       .toArray();
-
     if (!clients.length) {
-      console.log("No clients found");
       return new Response(JSON.stringify({ message: "No clients found" }), {
         status: 404,
         headers: { "Content-Type": "application/json" },
       });
     }
-
-    console.log("Fetching bookings for all clients");
     const bookings = await db.collection("Booking").find({}).toArray();
-
-    console.log("Fetching all trainers");
     const trainers = await db.collection("Trainers").find({}).toArray();
-
-    console.log("Enriching bookings with client and trainer data");
     const enrichedBookings = bookings.map((booking) => {
       const client = clients.find(
         (client) => client._id.toString() === booking.client_id.toString()
@@ -46,8 +35,6 @@ export async function GET() {
         bookedSlots: booking.bookedslots,
       };
     });
-
-    console.log("Returning data");
     return new Response(
       JSON.stringify({
         clients: clients.map((client) => ({
@@ -69,7 +56,6 @@ export async function GET() {
       }
     );
   } catch (error) {
-    console.error("Error fetching data:", error);
     return new Response(JSON.stringify({ error: "Internal server error" }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
